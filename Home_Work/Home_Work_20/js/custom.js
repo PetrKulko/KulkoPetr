9390e6322be5d56474595ff38c8e62cfedf85d0e
Petr_Kulko <petrokulko@gmail.com>
2021-07-05 13:29:02 +0300

    compleate Home work 20

---
+++ Home_Work/Home_Work_20/js/custom.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+jQuery.noConflict();
+jQuery(document).ready(function () {
+    "use strict";
+
+    var slid = jQuery("#carousel-reviews");
+
+    slid.owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        smartSpeed: 1000,
+        dotsSpeed: 1000,
+        loop: true,
+        nav: false,
+        navText: [
+            '<i class="fas fa-arrow-left"></i>',
+            '<i class="fas fa-arrow-right"></i>',
+        ],
+        dots: true,
+        mouseDrag: true,
+        margin: 30,
+        stagePadding: 0,
+        autoWidth: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+                mouseDrag: true,
+                autoplay: false
+            },
+            479: {
+                items: 1,
+                dots: true,
+                mouseDrag: true
+            },
+            767: {
+                items: 1,
+                dots: true,
+                mouseDrag: true
+            },
+            991: {
+                items: 1,
+                dots: true,
+                mouseDrag: true
+            },
+            1099: {
+                items: 1,
+                dots: true,
+                mouseDrag: true
+            },
+        },
+    });
+});
