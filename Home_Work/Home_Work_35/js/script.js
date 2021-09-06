b9cb525af54d37561bb2cf886477530267f5152e
PetrKulko <petrokulko@gmail.com>
2021-09-06 11:18:15 +0300

    compleate Home work 35

---
+++ Home_Work/Home_Work_35/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // open modal
+    $('.btn').click(function () {
+        $('.wrapper__modal').fadeIn();
+    });
+
+    // close modal
+    $('.modal__close').click(function () {
+        $(this).parents('.wrapper__modal').fadeOut();
+    });
+
+    $('.wrapper__modal').click(function (e) {
+        if ($(e.target).closest('.modal').length == 0) {
+            $(this).fadeOut();
+        }
+    });
+});
